e8bd6f2904ef8cc99063c07cd0f8adf4324d254b
Navapon <navapon.dev@gmail.com>
2025-01-25 00:20:29 +0700

    ci: trigger

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -75,6 +75,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   }
 }
 
+# Add comment
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
